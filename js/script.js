c4c2e53bd1595a59b4aa3bb5937f7fa2095aedc4
leoportogtr86 <leoporto_studio@hotmail.com>
2020-11-21 07:15:43 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,272 @@
+let titulo = document.getElementById('titulo')
+let campo = document.getElementById('campo')
+let c = document.getElementById('c')
+let db = document.getElementById('db')
+let d = document.getElementById('d')
+let eb = document.getElementById('eb')
+let e = document.getElementById('e')
+let f = document.getElementById('f')
+let gb = document.getElementById('gb')
+let g = document.getElementById('g')
+let ab = document.getElementById('ab')
+let a = document.getElementById('a')
+let bb = document.getElementById('bb')
+let b = document.getElementById('b')
+let grau1 = document.getElementById('grau1')
+let grau2 = document.getElementById('grau2')
+let grau3 = document.getElementById('grau3')
+let grau4 = document.getElementById('grau4')
+let grau5 = document.getElementById('grau5')
+let grau6 = document.getElementById('grau6')
+let grau7 = document.getElementById('grau7')
+let cifra1 = document.getElementById('cifra1')
+let cifra2 = document.getElementById('cifra2')
+let cifra3 = document.getElementById('cifra3')
+let cifra4 = document.getElementById('cifra4')
+let cifra5 = document.getElementById('cifra5')
+let cifra6 = document.getElementById('cifra6')
+let cifra7 = document.getElementById('cifra7')
+
+
+c.onclick = function () {
+
+    titulo.innerHTML = ' Campo Harmônico - C Maior'
+    campo.innerHTML = 'C Dm Em F G Am B°'
+    grau1.src = '../assets/img/c.svg'
+    grau2.src = '../assets/img/dm.svg'
+    grau3.src = '../assets/img/em.svg'
+    grau4.src = '../assets/img/f.svg'
+    grau5.src = '../assets/img/g.svg'
+    grau6.src = '../assets/img/am.svg'
+    grau7.src = '../assets/img/bdim.svg'
+
+    cifra1.innerHTML = 'C'
+    cifra2.innerHTML = 'Dm'
+    cifra3.innerHTML = 'Em'
+    cifra4.innerHTML = 'F'
+    cifra5.innerHTML = 'G'
+    cifra6.innerHTML = 'Am'
+    cifra7.innerHTML = 'B°'
+
+}
+
+
+
+db.onclick = function () {
+
+    titulo.innerHTML = ' Campo Harmônico - Db Maior'
+    campo.innerHTML = 'Db Ebm Fm Gb Ab Bbm C°'
+
+}
+
+
+d.onclick = function () {
+
+    titulo.innerHTML = ' Campo Harmônico - D Maior'
+    campo.innerHTML = 'D Em F#m G A Bm C#°'
+
+    grau1.src = '../assets/img/d.svg'
+    grau2.src = '../assets/img/em.svg'
+    grau3.src = '../assets/img/fsm.svg'
+    grau4.src = '../assets/img/g.svg'
+    grau5.src = '../assets/img/a.svg'
+    grau6.src = '../assets/img/bm.svg'
+    grau7.src = '../assets/img/csdim.svg'
+
+    cifra1.innerHTML = 'D'
+    cifra2.innerHTML = 'Em'
+    cifra3.innerHTML = 'F#m'
+    cifra4.innerHTML = 'G'
+    cifra5.innerHTML = 'A'
+    cifra6.innerHTML = 'Bm'
+    cifra7.innerHTML = 'C#°'
+
+}
+
+
+eb.onclick = function () {
+
+    titulo.innerHTML = ' Campo Harmônico - Eb Maior'
+    campo.innerHTML = 'Eb Fm Gm Ab Bb Cm D°'
+
+}
+
+
+e.onclick = function () {
+
+    titulo.innerHTML = ' Campo Harmônico - E Maior'
+    campo.innerHTML = 'E F#m G#m A B C#m D#°'
+
+    grau1.src = '../assets/img/e.svg'
+    grau2.src = '../assets/img/fsm.svg'
+    grau3.src = '../assets/img/gsm.svg'
+    grau4.src = '../assets/img/a.svg'
+    grau5.src = '../assets/img/b.svg'
+    grau6.src = '../assets/img/csm.svg'
+    grau7.src = '../assets/img/dsdim.svg'
+
+    cifra1.innerHTML = 'E'
+    cifra2.innerHTML = 'F#m'
+    cifra3.innerHTML = 'G#m'
+    cifra4.innerHTML = 'A'
+    cifra5.innerHTML = 'B'
+    cifra6.innerHTML = 'C#m'
+    cifra7.innerHTML = 'D#°'
+
+}
+
+
+f.onclick = function () {
+
+    titulo.innerHTML = ' Campo Harmônico - F Maior'
+    campo.innerHTML = 'F Gm Am Bb C Dm E°'
+
+    grau1.src = '../assets/img/f.svg'
+    grau2.src = '../assets/img/gm.svg'
+    grau3.src = '../assets/img/am.svg'
+    grau4.src = '../assets/img/bb.svg'
+    grau5.src = '../assets/img/c.svg'
+    grau6.src = '../assets/img/dm.svg'
+    grau7.src = '../assets/img/edim.svg'
+
+    cifra1.innerHTML = 'F'
+    cifra2.innerHTML = 'Gm'
+    cifra3.innerHTML = 'Am'
+    cifra4.innerHTML = 'Bb'
+    cifra5.innerHTML = 'C'
+    cifra6.innerHTML = 'Dm'
+    cifra7.innerHTML = 'E°'
+
+}
+
+
+gb.onclick = function () {
+
+    titulo.innerHTML = ' Campo Harmônico - Gb Maior'
+    campo.innerHTML = 'Gb Abm Bbm Cb Db Ebm F°'
+
+    grau1.src = '../assets/img/fs.svg'
+    grau2.src = '../assets/img/gsm.svg'
+    grau3.src = '../assets/img/asm.svg'
+    grau4.src = '../assets/img/b.svg'
+    grau5.src = '../assets/img/db.svg'
+    grau6.src = '../assets/img/ebm.svg'
+    grau7.src = '../assets/img/fdim.svg'
+
+    cifra1.innerHTML = 'Gb'
+    cifra2.innerHTML = 'Abm'
+    cifra3.innerHTML = 'Bbm'
+    cifra4.innerHTML = 'Cb'
+    cifra5.innerHTML = 'Db'
+    cifra6.innerHTML = 'Ebm'
+    cifra7.innerHTML = 'F°'
+
+}
+
+
+g.onclick = function () {
+
+    titulo.innerHTML = ' Campo Harmônico - G Maior'
+    campo.innerHTML = 'G Am Bm C D Em F#°'
+    grau1.src = '../assets/img/g.svg'
+    grau2.src = '../assets/img/am.svg'
+    grau3.src = '../assets/img/bm.svg'
+    grau4.src = '../assets/img/c.svg'
+    grau5.src = '../assets/img/d.svg'
+    grau6.src = '../assets/img/em.svg'
+    grau7.src = '../assets/img/fsdim.svg'
+
+    cifra1.innerHTML = 'G'
+    cifra2.innerHTML = 'Am'
+    cifra3.innerHTML = 'Bm'
+    cifra4.innerHTML = 'C'
+    cifra5.innerHTML = 'D'
+    cifra6.innerHTML = 'Em'
+    cifra7.innerHTML = 'F#°'
+
+}
+
+
+ab.onclick = function () {
+
+    titulo.innerHTML = ' Campo Harmônico - Ab Maior'
+    campo.innerHTML = 'Ab Bbm Cm Db Eb Fm G°'
+
+}
+
+
+a.onclick = function () {
+
+    titulo.innerHTML = ' Campo Harmônico - A Maior'
+    campo.innerHTML = 'A Bm C#m D E F#m G#°'
+
+    grau1.src = '../assets/img/a.svg'
+    grau2.src = '../assets/img/bm.svg'
+    grau3.src = '../assets/img/csm.svg'
+    grau4.src = '../assets/img/d.svg'
+    grau5.src = '../assets/img/e.svg'
+    grau6.src = '../assets/img/fsm.svg'
+    grau7.src = '../assets/img/gsdim.svg'
+
+    cifra1.innerHTML = 'A'
+    cifra2.innerHTML = 'Bm'
+    cifra3.innerHTML = 'C#m'
+    cifra4.innerHTML = 'D'
+    cifra5.innerHTML = 'E'
+    cifra6.innerHTML = 'F#m'
+    cifra7.innerHTML = 'G#°'
+
+}
+
+
+bb.onclick = function () {
+
+    titulo.innerHTML = ' Campo Harmônico - Bb Maior'
+    campo.innerHTML = 'Bb Cm Dm Eb F Gm A°'
+
+    grau1.src = '../assets/img/bb.svg'
+    grau2.src = '../assets/img/cm.svg'
+    grau3.src = '../assets/img/dm.svg'
+    grau4.src = '../assets/img/eb.svg'
+    grau5.src = '../assets/img/f.svg'
+    grau6.src = '../assets/img/gm.svg'
+    grau7.src = '../assets/img/adim.svg'
+
+    cifra1.innerHTML = 'Bb'
+    cifra2.innerHTML = 'Cm'
+    cifra3.innerHTML = 'Dm'
+    cifra4.innerHTML = 'Eb'
+    cifra5.innerHTML = 'F'
+    cifra6.innerHTML = 'Gm'
+    cifra7.innerHTML = 'A°'
+
+}
+
+
+b.onclick = function () {
+
+    titulo.innerHTML = ' Campo Harmônico - B Maior'
+    campo.innerHTML = 'B C#m D#m E F# G#m A#°'
+
+    grau1.src = '../assets/img/b.svg'
+    grau2.src = '../assets/img/csm.svg'
+    grau3.src = '../assets/img/dsm.svg'
+    grau4.src = '../assets/img/e.svg'
+    grau5.src = '../assets/img/fs.svg'
+    grau6.src = '../assets/img/gsm.svg'
+    grau7.src = '../assets/img/asdim.svg'
+
+    cifra1.innerHTML = 'B'
+    cifra2.innerHTML = 'C#m'
+    cifra3.innerHTML = 'D#m'
+    cifra4.innerHTML = 'E'
+    cifra5.innerHTML = 'F#'
+    cifra6.innerHTML = 'G#m'
+    cifra7.innerHTML = 'A#°'
+
+}
+
+$('.carousel').carousel({
+
+    interval: 1000 * 60
+})
